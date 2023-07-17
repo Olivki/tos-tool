3043ac336a6ee27590b5f7f134bbce05092eea87
Oliver Berg <oliver@berg.moe>
2023-07-17 18:26:44 +0200

    Remove `ies` commands using old system

--- tos-cli/src/main/kotlin/net/ormr/tos/cli/main.kt
+++ tos-cli/src/main/kotlin/net/ormr/tos/cli/main.kt
@@ -18,15 +18,12 @@ package net.ormr.tos.cli
 
 import com.github.ajalt.clikt.core.subcommands
 import net.ormr.tos.cli.ies.IesCommand
-import net.ormr.tos.cli.ies.IesPackCommand
-import net.ormr.tos.cli.ies.IesTestCommand
-import net.ormr.tos.cli.ies.IesUnpackCommand
 import net.ormr.tos.cli.ipf.IpfCommand
 import net.ormr.tos.cli.ipf.IpfUnpackCommand
 
 fun main(args: Array<String>) = TosCommand()
     .subcommands(
-        IesCommand().subcommands(IesUnpackCommand(), IesPackCommand(), IesTestCommand()),
+        IesCommand().subcommands(),
         IpfCommand().subcommands(IpfUnpackCommand()),
     )
     .main(args)
